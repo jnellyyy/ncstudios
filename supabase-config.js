df205aae4ce44954e23fd5819ad973ebad561eaf
jnellyyy <janelle.lamptey@gmail.com>
2026-05-31 16:25:33 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
-const NC_SUPABASE_URL = "https://qimgavpfscppnlsbxjhbk.supabase.co";
+const NC_SUPABASE_URL = "https://qimgavpfscpnlsbxjhbk.supabase.co";
 
-const NC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpbWdhdnBmc2NwbmxzYnhqaGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMjQ5NjYsImV4cCI6MjA5NTgwMDk2Nn0.fFR3oMeU15sn0FUNQGn769M_xQlrcDylstn4SHYvKcg";
+const NC_SUPABASE_ANON_KEY = "sb_publishable_CIR45TS0FmiV_RiQZBqhfg_-mDFUqT7";
 
 window.ncSupabase = window.supabase.createClient(
   NC_SUPABASE_URL,
